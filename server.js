9a0af43c87e14f99ccad312431efd376e3ed73ea
Luvkitri <janjedrzejewski@gmail.com>
2020-11-30 14:14:16 +0100

    Main server file

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+
+// Load env variables
+dotenv.config({ path: './config/.env' });
+
+let app = express();
+
+// * Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Routes
+app.use('/entries', require('./routes/entries'));
+app.use('/entries', require('./routes/tags'));
+
+const port = process.env.PORT;
+let server = app.listen(port, () => {
+    console.log(`Listening at port ${port}`);
+});

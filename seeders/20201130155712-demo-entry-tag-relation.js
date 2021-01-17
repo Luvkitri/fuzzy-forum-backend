da1cd6e0805306121e731adb038c9036863a8a3e
Luvkitri <janjedrzejewski@gmail.com>
2021-01-17 18:52:55 +0100

    Seeds updated for fuzzy logic testing

--- seeders/20201130155712-demo-entry-tag-relation.js
+++ seeders/20201130155712-demo-entry-tag-relation.js
@@ -37,6 +37,90 @@ module.exports = {
                 tag_id: 3
             }
         ]);
+
+        // Java
+        await queryInterface.bulkInsert('entry_tag_relation', [
+            {
+                entry_id: 6,
+                tag_id: 4
+            },
+            {
+                entry_id: 7,
+                tag_id: 4
+            },
+            {
+                entry_id: 8,
+                tag_id: 4
+            },
+            {
+                entry_id: 9,
+                tag_id: 4
+            },
+            {
+                entry_id: 10,
+                tag_id: 4
+            },
+            {
+                entry_id: 11,
+                tag_id: 4
+            },
+            {
+                entry_id: 12,
+                tag_id: 4
+            },
+            {
+                entry_id: 13,
+                tag_id: 4
+            },
+            {
+                entry_id: 14,
+                tag_id: 4
+            },
+            {
+                entry_id: 15,
+                tag_id: 4
+            },
+            {
+                entry_id: 16,
+                tag_id: 4
+            },
+            {
+                entry_id: 17,
+                tag_id: 4
+            },
+            {
+                entry_id: 18,
+                tag_id: 4
+            },
+        ]);
+
+        // C++
+        await queryInterface.bulkInsert('entry_tag_relation', [
+            {
+                entry_id: 19,
+                tag_id: 5
+            },
+            {
+                entry_id: 20,
+                tag_id: 5
+            },
+            {
+                entry_id: 21,
+                tag_id: 5
+            },
+            {
+                entry_id: 22,
+                tag_id: 5
+            },
+            {
+                entry_id: 23,
+                tag_id: 5
+            },
+            {
+                entry_id: 24,
+                tag_id: 5
+            },
+        ]);
     },
 
     down: async (queryInterface, Sequelize) => {
